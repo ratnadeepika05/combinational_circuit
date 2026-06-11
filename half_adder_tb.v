1a72b3b0a045018293aa7a144566c176f2031340
ratnadeepika05 <ratnadeepikaboosa@gmail.com>
2026-06-12 01:27:27 +0530

    Add files via upload

---
+++ half_adder_tb.v
@@ -0,0 +1,25 @@
+module half_adder_tb;
+
+reg a,b;
+wire s,cy;
+
+half_adder uut(
+    .a(a),
+    .b(b),
+    .s(s),
+    .cy(cy)
+);
+
+initial begin
+    $dumpfile("half_adder.vcd");
+    $dumpvars(0,half_adder_tb);
+
+    a=0; b=0;
+    #10 a=0; b=1;
+    #10 a=1; b=0;
+    #10 a=1; b=1;
+
+    #10 $finish;
+end
+
+endmodule
